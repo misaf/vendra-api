783db9ad910e24ea1a22f2596d4827353408634d
misaf <misaf.1990@gmail.com>
2026-07-11 20:47:02 +0000

    wip

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 arch()->preset()->php();
 arch()->preset()->security();
 arch()->preset()->laravel();
+
+arch('shared api infrastructure never depends on a concrete tenant provider')
+    ->expect('Misaf\VendraApi')
+    ->not->toUse('Misaf\VendraTenant');
